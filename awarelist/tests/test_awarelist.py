b54d79bc1c19f956d91011c4981dfa1afaa1c2d7
Guillaume Pannatier <guillaume.pannatier@gmail.com>
2015-03-24 10:52:33 +0100

    Indent using space

--- awarelist/tests/test_awarelist.py
+++ awarelist/tests/test_awarelist.py
@@ -5,63 +5,63 @@ import unittest
 import awarelist
 
 class TestAwareList(unittest.TestCase):
-	def test_logic(self):
-		aw_list = awarelist.AwareList(["InitialA", "InitialB"])
-		self.assertEqual(aw_list.added, [])
-		self.assertEqual(aw_list.removed, [])
+    def test_logic(self):
+        aw_list = awarelist.AwareList(["InitialA", "InitialB"])
+        self.assertEqual(aw_list.added, [])
+        self.assertEqual(aw_list.removed, [])
 
-		aw_list.append('MyFirstAppend')
-		aw_list.insert(0, 'MyFirstInsert')
+        aw_list.append('MyFirstAppend')
+        aw_list.insert(0, 'MyFirstInsert')
 
-		self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend"])
-		self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert"])
-		self.assertEqual(aw_list.removed, [])
+        self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend"])
+        self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert"])
+        self.assertEqual(aw_list.removed, [])
 
-		aw_list.extend(["ExtendA", "ExtendB"])
-		self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend", "ExtendA", "ExtendB"])
-		self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert", "ExtendA", "ExtendB"])
-		self.assertEqual(aw_list.removed, [])
+        aw_list.extend(["ExtendA", "ExtendB"])
+        self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend", "ExtendA", "ExtendB"])
+        self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert", "ExtendA", "ExtendB"])
+        self.assertEqual(aw_list.removed, [])
 
-		aw_list.remove('ExtendA')
-		self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend", "ExtendB"])
-		aw_list.pop()
-		self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend"])
-		self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert"])
-		self.assertEqual(aw_list.removed, [])
+        aw_list.remove('ExtendA')
+        self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend", "ExtendB"])
+        aw_list.pop()
+        self.assertEqual(aw_list, ["MyFirstInsert", "InitialA", "InitialB", "MyFirstAppend"])
+        self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert"])
+        self.assertEqual(aw_list.removed, [])
 
-		aw_list.remove('InitialA')
-		self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert"])
-		self.assertEqual(aw_list.removed, ['InitialA'])
+        aw_list.remove('InitialA')
+        self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert"])
+        self.assertEqual(aw_list.removed, ['InitialA'])
 
-		aw_list.append('InitialB')
-		self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert", "InitialB"])
+        aw_list.append('InitialB')
+        self.assertEqual(aw_list.added, ["MyFirstAppend", "MyFirstInsert", "InitialB"])
 
-	def test_magic(self):
-		aw_list = awarelist.AwareList(['A', 'B', 'C'])
-		self.assertEqual(aw_list.added, [])
-		self.assertEqual(aw_list.removed, [])
+    def test_magic(self):
+        aw_list = awarelist.AwareList(['A', 'B', 'C'])
+        self.assertEqual(aw_list.added, [])
+        self.assertEqual(aw_list.removed, [])
 
-		aw_list.append('new')
-		self.assertEqual(aw_list.added, ['new'])
-		self.assertEqual(aw_list.removed, [])
-		self.assertEqual(aw_list, ['A','B','C','new'])
+        aw_list.append('new')
+        self.assertEqual(aw_list.added, ['new'])
+        self.assertEqual(aw_list.removed, [])
+        self.assertEqual(aw_list, ['A','B','C','new'])
 
-		del aw_list[:]
-		self.assertEqual(aw_list, [])
-		self.assertEqual(aw_list.added, [])
-		self.assertEqual(aw_list.removed, ['A','B','C'])
+        del aw_list[:]
+        self.assertEqual(aw_list, [])
+        self.assertEqual(aw_list.added, [])
+        self.assertEqual(aw_list.removed, ['A','B','C'])
 
-		aw_list = awarelist.AwareList(['A'])
-		aw_list += ['Yop']
-		self.assertEqual(aw_list, ['A', 'Yop'])
-		self.assertEqual(aw_list.added, ['Yop'])
+        aw_list = awarelist.AwareList(['A'])
+        aw_list += ['Yop']
+        self.assertEqual(aw_list, ['A', 'Yop'])
+        self.assertEqual(aw_list.added, ['Yop'])
 
-		aw_list += "ABC"
-		self.assertEqual(aw_list, ['A', 'Yop', 'A', 'B', 'C'])
-		self.assertEqual(aw_list.added, ['Yop', 'A', 'B', 'C'])
+        aw_list += "ABC"
+        self.assertEqual(aw_list, ['A', 'Yop', 'A', 'B', 'C'])
+        self.assertEqual(aw_list.added, ['Yop', 'A', 'B', 'C'])
 
-		with self.assertRaises(TypeError):
-			aw_list += 1
-		
+        with self.assertRaises(TypeError):
+            aw_list += 1
+        
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
